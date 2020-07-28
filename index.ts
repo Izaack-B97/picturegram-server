3e2cfefe58c3d70e841a1fd73828a584ee34be8b
Isaac Bustamante <izaack.97@gmail.com>
2020-07-27 17:05:00 -0700

    Commit inicial - Listo para trabajar

---
+++ index.ts
@@ -0,0 +1,11 @@
+/**
+ * express: Servidor web
+ * body-parser: Recibe los datos de una peticion post y lo convierte en un json
+ * cors: Puede hacer peticiones cross domain
+ * moongose: Te permite interactuar con la db
+ * express-fileupload: Permite recibir peticiones de archivos
+ * jsonwebtokens: Es para la autenticacion de la app
+ * bcrypt: Encripta las contraseñas de los usuarios
+ */
+
+console.log('Hola isaac');
